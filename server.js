0d6641b6bfebc0ce0ba93541ea2352d413bb6814
Vidush Goswami <goswamiv@grinnell.edu>
2021-11-13 13:28:42 -0600

    setup server

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require ('express')
+const app = express()
+
+
+var requestTime = function(req,res,next){
+    req.requestTime = Date.now()
+    next();
+}
+
+app.use(requestTime)
+
+app.get('/api/hello', function (req,res, next){
+    res.subMessage = 'world'
+    next();
+},  function (req,res){
+    res.json({message:'helloooo '+res.subMessage+ ' it is currently '+req.requestTime})
+})
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, function(){
+    console.log('Now listening on port '+port);
+})
+
